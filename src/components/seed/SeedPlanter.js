7b4dfe93c6bf8191dadc6a38f6a9e9750400bbd9
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-20 22:30:29 -0500

    [sm] fixed tree overlap bug and added color picker functionality

--- src/components/seed/SeedPlanter.js
+++ src/components/seed/SeedPlanter.js
@@ -5,6 +5,7 @@ const SeedPlanter = () => {
     const [ trees, setTrees ] = useState([]);
     const [ drawTrees, setDrawTrees ] = useState([]);
     const [ diameter, setDiameter ] = useState(10);
+    const [ color, setColor ] = useState("#86cf38");
 
     useEffect(() => {
         // console.log(mouseX, mouseY);
@@ -13,8 +14,8 @@ const SeedPlanter = () => {
     useEffect(() => {
         setDrawTrees(trees.map(tree => {
             return <div className="absolute shadow" style={{
-                top: tree.y - 2, 
-                left: tree.x - 2, 
+                top: tree.y, 
+                left: tree.x, 
                 borderRadius: "50%",
                 height: `${tree.diameter}px`,
                 width: `${tree.diameter}px`,
@@ -34,28 +35,38 @@ const SeedPlanter = () => {
         setDiameter(e.target.value);
     }
 
+    const handleColor = (e) => {
+        setColor(e.target.value);
+    }
+
     const plant = (e) => {
         console.log(`tree planted at ${mouse.x}, ${mouse.y}`)
         setTrees(trees => [...trees, {
-            x: mouse.x,
-            y: mouse.y,
+            x: mouse.x - (diameter/2),
+            y: mouse.y - (diameter/2),
             diameter: diameter ? diameter : 2,
-            age: diameter ? diameter : 2,
-            color: "rgb(50, 200, 50)"
+            age: 0,
+            color: color
         }])
     }
 
     return (
     <div className="w-full pt-24 grid grid-cols-3">
-        <div className="button-panel w-max mx-auto">
-        {`Diameter: `}
-        <input className="border mb-2" value={diameter}  onChange={(e) => handleDiameter(e)} type="number" />
-
+        <div> {/* LEFT COL - BUTTON PANEL */}
+            <div className="button-panel w-max ml-24">
+            {`Diameter: `}
+            <input className="border mb-2" value={diameter}  onChange={(e) => handleDiameter(e)} type="number" />
+            </div>
+            <div className="button-panel w-max ml-24">
+                <label for="head">Active Color: </label>
+                <input type="color" id="head" name="head" onChange={(e) => handleColor(e)} value={color}/>
+            </div>
         </div>
-        <div className="mx-auto bg-green-200 w-72 h-72 relative overflow-hidden" 
-            onMouseMove={(e) => _onMouseMove(e)}
-            onClick={(e) => plant(e)}
-            >{drawTrees}</div>
+        <div className="mx-auto w-72 h-72 z-10 bg-white relative overflow-hidden border" onMouseMove={(e) => _onMouseMove(e)} onClick={(e) => plant(e)}> { /* Gameboard */ } 
+            <div className="absolute opacity-0 bg-black z-10 w-72 h-72"></div>
+            {drawTrees} 
+            <div className="mx-auto bg-green-200 w-72 h-72 overflow-hidden"></div>
+        </div>
         <div className="border mx-auto border w-72 h-12 relative">
             <div className="absolute top-2 left-4 flex">
                 <p className="mr-1 text-gray-400">mouseX:</p>
