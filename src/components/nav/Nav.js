c501b984ed15a8c785969a2486158c6faa2728d1
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-20 20:14:32 -0500

    [sm] reworked the Nav menu names and urls

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,10 +4,9 @@ import { NavLink } from 'react-router-dom';
 const Nav = () => {
     return (
         <div className="flex justify-around">
-            <NavLink exact className="flex-grow bg-blue-400 py-2 px-4 text-blue-50 hover:bg-gray-50 hover:text-gray-500" to="/" activeClassName="border-b-4">Home</NavLink>
-            <NavLink exact className="flex-grow bg-blue-400 py-2 px-4 text-blue-50 hover:bg-gray-50 hover:text-gray-500" to="/about" activeClassName="border-b-4">About</NavLink>
-            <NavLink exact className="flex-grow bg-blue-400 py-2 px-4 text-blue-50 hover:bg-gray-50 hover:text-gray-500" to="/stargame" activeClassName="border-b-4">Star Game</NavLink>
+            <NavLink exact className="flex-grow bg-blue-400 py-2 px-4 text-blue-50 hover:bg-gray-50 hover:text-gray-500" to="/" activeClassName="border-b-4">Home/About</NavLink>
             <NavLink exact className="flex-grow bg-blue-400 py-2 px-4 text-blue-50 hover:bg-gray-50 hover:text-gray-500" to="/seed-planter" activeClassName="border-b-4">Seed Planter</NavLink>
+            <NavLink exact className="flex-grow bg-blue-400 py-2 px-4 text-blue-50 hover:bg-gray-50 hover:text-gray-500" to="/painter-classic" activeClassName="border-b-4">Tree Painter Classic</NavLink>
         </div>
     )
 }
