20f451b79e3009c46f46b54da53edb42db12a27a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-21 12:54:58 -0500

    [sm] added a link and explanation for classic

--- src/components/classic/Classic.js
+++ src/components/classic/Classic.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Classic = () => {
     return (
-        <p>coming soon</p>
+        <>
+        <p>I'll embed it here later it is a version I was working on in processing</p>
+        <a className="text-gray-400 hover:text-blue-400" href="https://gostugo.com/treepainter.html">here is a link to it.</a>
+        </>
     )
 }
 
