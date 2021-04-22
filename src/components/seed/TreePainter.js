3111776d7d79dc1e82da39bd98970d44128db0aa
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-22 18:20:20 -0500

    [sm] added seed concept and countdown also made clear give you new seeds

--- src/components/seed/TreePainter.js
+++ src/components/seed/TreePainter.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 
 const TreePainter = () => {
     const [ mouse, setMouse] = useState({ x: 0, y: 0});
+    const [ seeds, setSeeds ] = useState(10);
     const [ trees, setTrees ] = useState([]);
     const [ drawTrees, setDrawTrees ] = useState([]);
-    const [ diameter, setDiameter ] = useState(10);
-    const [ color, setColor ] = useState("#86cf38");
+    const [ diameter, setDiameter ] = useState(15);
+    const [ color, setColor ] = useState("#B6FE90");
 
     useEffect(() => {
     },[mouse])
@@ -39,29 +40,38 @@ const TreePainter = () => {
     }
 
     const plant = (e) => {
-        // console.log(`tree planted at ${mouse.x}, ${mouse.y}`)
-        setTrees(trees => [...trees, {
-            x: mouse.x - (diameter/2),
-            y: mouse.y - (diameter/2),
-            diameter: diameter ? diameter : 2,
-            age: 0,
-            color: color
-        }])
+        if (seeds > 0) {
+
+            setTrees(trees => [...trees, {
+                x: mouse.x - (diameter/2),
+                y: mouse.y - (diameter/2),
+                diameter: diameter ? diameter : 2,
+                age: 0,
+                color: color
+            }]);
+            setSeeds(seeds - 1);
+        }
+    }
+
+    const reset = (e) => {
+        setTrees([])
+        setSeeds(10);
     }
 
     return (
     <div className="max-w-7xl mx-auto"> {/* GRID WRAPPER */}
         <div className="w-full pt-12 md:pt-24 grid  md:grid-cols-3">
-            <div className="w-72 mx-auto"> {/* LEFT COL - BUTTON PANEL */}
-                <div className="button-panel w-max md:ml-24">
+            <div className="w-72 md:ml-32 mx-auto"> {/* LEFT COL - BUTTON PANEL */}
+                <div>{ seeds !== 1 ? `You have ${seeds} seeds.` : `You have 1 seed.`}</div>
+                <div className="button-panel mt-2 w-max">
                 {`Diameter: `}
                 <input className="border w-12 mb-2" value={diameter}  onChange={(e) => handleDiameter(e)} type="number" />
                 </div>
-                <div className="button-panel w-max md:ml-24">
+                <div className="button-panel w-max ">
                     <label htmlFor="head">Active Color: </label>
                     <input type="color" id="head" name="head" onChange={(e) => handleColor(e)} value={color}/>
                 </div>
-                <button className="md:ml-24 border rounded px-2 py-1 mt-2" type="button" onClick={(e) => setTrees([])}>CLEAR</button>
+                <button className=" border rounded px-2 py-1 mt-2" type="button" onClick={(e) => reset(e)}>CLEAR</button>
             </div>
             <div className="mx-auto w-72 h-72 mt-2 z-10 bg-white relative overflow-hidden border" onMouseMove={(e) => _onMouseMove(e)} onClick={(e) => plant(e)}> { /* Gameboard */ } 
                 <div className="absolute opacity-0 bg-black z-10 w-72 h-72"></div>
