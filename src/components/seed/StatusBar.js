b24b4bc2f96e60a6793623c5bdd567960329c753
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-25 15:39:49 -0500

    added mystery icons to show acorn count

--- src/components/seed/StatusBar.js
+++ src/components/seed/StatusBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import AcornIcon from './AcornIcon';
 
 const StatusBar = ({seeds, water, stars,}) => {
 
     return (
         <div className="w-full bg-green-200">
-            <div className="flex max-w-7xl mx-auto justify-center">
-                <div className="px-1">{`seeds:${seeds}`}</div>
-                <div className="px-1">{`water:${water}`}</div>
-                <div className="px-1">{`stars:${stars}`}</div>
+            <div className="flex max-w-7xl mx-auto justify-center items-top">
+                <AcornIcon amount={seeds} />
+                <div className="px-1 pt-1">{`:${seeds}`}</div>
+                <div className="px-1 pt-1">{`water:${water}`}</div>
+                <div className="px-1 pt-1">{`stars:${stars}`}</div>
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'acorn-top': '#ECA76D',
+        'acorn-bottom': '#945E42'
+      }
+    },
   },
   variants: {
     extend: {},
