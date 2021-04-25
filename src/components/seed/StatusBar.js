efe1098e1e94f558a95ec8453596dfdbcd7dc310
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-04-24 20:09:22 -0500

    [sm] filled out some status bar logic

--- src/components/seed/StatusBar.js
+++ src/components/seed/StatusBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const StatusBar = () => {
+const StatusBar = ({seeds, water, stars,}) => {
 
     return (
-        <>
-        </>
+        <div className="flex w-full bg-green-200">
+            <div>{`seeds:${seeds}`}</div>
+            <div>{`water:${water}`}</div>
+            <div>{`stars:${stars}`}</div>
+        </div>
     )
 }
 
