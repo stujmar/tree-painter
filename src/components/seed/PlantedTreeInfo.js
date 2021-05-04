4222e31fc9e3ddfc21edc22ff4a2c24bfa71748e
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-03 20:42:53 -0500

    [sm] cleaned up CSS of cutdown debug button, eliminated redundant border property on parent

--- src/components/seed/PlantedTreeInfo.js
+++ src/components/seed/PlantedTreeInfo.js
@@ -3,13 +3,13 @@ import React from 'react'
 const PlantedTreeInfo = ({ x, y, color, age, id, diameter, onDelete }) => {
     return (
         <>
-            <div className="border p-1 m-1 rounded shadow" style={{borderColor: color, borderWidth: "3px"}}>
+            <div className="p-1 m-1 rounded shadow" style={{borderColor: color, borderWidth: "3px"}}>
                 <div className="flex"> {/* TOP ROW */}
                     <div className="ml-1">{`diameter: ${diameter}`}</div>
                     <div className="ml-2">{`age: ${age}`}</div>
                 </div>
                 <div className="flex"> {/* BOTTOM ROW*/}
-                    <button className="border rounded px-1 mx-1 hover:bg-gray-50" onClick={() => onDelete(id)}>cut down</button>
+                    <button className="border rounded px-1 mx-1 text-sm font-medium uppercase hover:bg-gray-50" onClick={() => onDelete(id)}>cut down</button>
                 </div>
             </div>
         </>
