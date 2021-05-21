61ea919557004229aeb2758057b102452d2e8886
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-20 21:33:18 -0500

    [sm] destructured props so it actually works

--- src/components/seed/Sunset.js
+++ src/components/seed/Sunset.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { defaultProps } from 'recompose';
 
-const Sunset = (opacity=1) => {
+const Sunset = ({opacity}) => {
     // let test = .5;
     return (
         <div 
         style={{ background: `linear-gradient(0deg, rgba(255,0,0,${opacity}) 0%, rgba(255,255,255,0) 85%)` }}
         className="absolute bottom-0 w-96 h-24 z-10">
-
         </div>
     )
 }
