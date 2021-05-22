2406bed1849f11d8924b4cba37493c71e64d9a15
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-21 20:44:21 -0500

    [sm] possibly setup the counter with custom useInterval hook working correctly

--- src/components/seed/Counter.js
+++ src/components/seed/Counter.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Counter = (props) => {
-    return (
-        <></>
-    )
-}
+    let [count, setCount] = useState(0);
 
-export default Counter
+    function useInterval(callback, delay) {
+        const savedCallback = useRef();
+      
+        // Remember the latest callback.
+        useEffect(() => {
+          savedCallback.current = callback;
+        }, [callback]);
+      
+        // Set up the interval.
+        useEffect(() => {
+          function tick() {
+            savedCallback.current();
+          }
+          if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+          }
+        }, [delay]);
+    }
+    useInterval(() => {
+      // Your custom logic here
+      setCount(count + 1);
+    }, 1000);
+  
+    return <h1>{count}</h1>;
+ }
 
-
+export default Counter;
