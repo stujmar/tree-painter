843d9c6e3ae2c786b44ee81b1129be998b35f05a
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-21 20:48:14 -0500

    [sm] added a counter to the app and made it pinned to the top left corner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Counter from './components/seed/Counter';
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Classic from './components/classic/Classic';
 
@@ -18,6 +19,9 @@ const App = () => {
     <div className="App">
       <header className="bg-green-600 h-36 flex items-center justify-center text-lg font-sans text-white">
         <p>{message}</p>
+        <div className="p-4 absolute top-0 left-0">
+          <Counter />
+        </div>
       </header>
       <TreePainter messageChange={(e) => handleMessageChange(e)}/>
       {/* <Router>
