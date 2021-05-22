1beb2407d543f11a8dbe154fc8153f963265f738
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-05-22 16:27:49 -0500

    [sm] made sky clickable, broke tree placement thought

--- src/components/seed/sky.js
+++ src/components/seed/sky.js
@@ -2,10 +2,16 @@ import React from 'react';
 import Sunset from './Sunset'
 
 const Sky = ({ hour }) => {
+    const clickSky = () => {
+        console.log("Sky");
+    }
+
     return (
-        <div className="mx-auto lg:mr-auto bg-blue-400 w-96 relative" style={{height: 100}}>
+        <button 
+            onClick={clickSky}
+            className="mx-auto lg:mr-auto bg-blue-400 w-96 relative focus:outline-none" style={{height: 100}}>
             <Sunset opacity={.5} />
-        </div> 
+        </button> 
     )   
 }
 
