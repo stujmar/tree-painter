52e90cdb474b03ca1bcbba6bf6ad3f410719d1f8
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-05 22:09:05 -0500

    [sm] componentize the tree button, but didn't implement in TreePainter.js

--- src/components/seed/Tree.js
+++ src/components/seed/Tree.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const Tree = () => {
+const Tree = (props) => {
     return (
-        <></>
+        <button key={props.id} 
+            className={`absolute origin-bottom-center focus:outline-none ${ water > 0 ? "water-cursor" : "no-water-cursor" }`}
+            onClick={() => { props.waterTree(props.id)} } 
+            style={{
+                top: props.y, 
+                left: props.x, 
+                borderRadius: "0%",
+                borderBottom: `${props.diameter * 1.25}px solid ${props.color}`,
+                borderLeft: `${props.diameter}px solid rgba(0,0,0,0)`,
+                borderRight: `${props.diameter}px solid rgba(0,0,0,0)`,
+                height: `${props.diameter}px`,
+                width: `${props.diameter}px`,
+                }}></button>
     )
 }
 
