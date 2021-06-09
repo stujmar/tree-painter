f001dd1a68ccca96554b6ace8bba40ab59c57b16
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-08 22:30:01 -0500

    [sm] added update and get action types for seasons

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -1,6 +1,8 @@
 const actionTypes = {
     UPDATE_DAY: "UPDATE_DAY",
-    GET_DAY: "GET_DAY"    
+    GET_DAY: "GET_DAY",
+    UPDATE_SEASON: "UPDATE_SEASON",
+    GET_SEASON: "GET_SEASON"
 };
 
 export default actionTypes;
