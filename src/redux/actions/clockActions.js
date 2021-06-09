4b272a9c25508ecf7ad11db5bcf78d7640a31e12
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-08 22:59:49 -0500

    [sm] added updateDay finction to the clockActions.js

--- src/redux/actions/clockActions.js
+++ src/redux/actions/clockActions.js
@@ -2,4 +2,8 @@ import actionTypes from './actionTypes';
 
 export function updateHour(hour) {
     return {type: actionTypes.UPDATE_HOUR, hour} // Object shorthand syntax ;) 
+}
+
+export function updateDay(day) {
+    return {type: actionTypes.UPDATE_DAY, day} // Object shorthand syntax ;) 
 } 
