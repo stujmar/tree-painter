75fa7a9466570a1b9347432e1e672789cdc43e04
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-09 19:54:27 -0500

    [sm] added updated logic to the store and renamed slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,12 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import hoursReducer from './hoursSlice';
+import daysReducer from './daysSlice';
+import seasonsReducer from './seasonsSlice';
 
-// export const store = configureStore({
-//     reducer: {
-//         counter: counterReducer,
-//     },
-// });
+export const store = configureStore({
+    reducer: {
+        hours: hoursReducer,
+        days: daysReducer,
+        seaons: seasonsReducer
+    },
+});
