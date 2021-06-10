07b53124a8adba19a3dd83672d5c1c752373b0c1
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-09 21:45:09 -0500

    [sm] got updating redux state to work but broke just about everything else ;)

--- src/App.js
+++ src/App.js
@@ -2,34 +2,35 @@ import React, { useEffect, useState } from 'react';
 import Counter from './components/seed/Counter';
 import { useSelector, useDispatch } from 'react-redux';
 import TreePainter from './components/seed/TreePainter';
-import { selectSeason } from './redux/seasonSlice';
+import { selectSeason, setSeason } from './redux/seasonSlice';
+import { increment } from './redux/daySlice';
 
 const App = () => {
   const activeSeason = useSelector(selectSeason);
   const dispatch = useDispatch();
 
   const [ message, setMessage ] = useState("");
-  const [ season, setSeason ] = useState("SPRING");
+  // const [ season, setSeason ] = useState("SPRING");
 
   const handleMessageChange = (payload) => {
     setMessage(payload);
   }
 
 
-  const getSeason = () => {
-    switch(season) {
-      case "SPRING":
-        return "bg-green-400";
-      case "SUMMER":
-        return "bg-yellow-400";
-      case "FALL":
-        return "bg-orange-400";
-      case "WINTER":
-        return "bg-blue-400";
-      default:
-        // code block
-    }
-  };
+  // const getSeason = () => {
+  //   switch(season) {
+  //     case "SPRING":
+  //       return "bg-green-400";
+  //     case "SUMMER":
+  //       return "bg-yellow-400";
+  //     case "FALL":
+  //       return "bg-orange-400";
+  //     case "WINTER":
+  //       return "bg-blue-400";
+  //     default:
+  //       // code block
+  //   }
+  // };
 
   const logSeason = () => {
     console.log(activeSeason);
@@ -39,9 +40,9 @@ const App = () => {
     dispatch(setSeason("WINTER"));
   };
   const fall = () => {
-    dispatch(setSeason("FALL"));
+    dispatch(increment());
   };
-
+/*
   useEffect(() => {
     getSeason();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -51,19 +52,20 @@ const App = () => {
     setSeason(_season);
     console.log(_season, "from app level");
   }
-
+*/
   return (
     <div className="App">
-      <header className={`${getSeason()} h-48 flex items-center justify-center text-lg font-sans text-white`}>
+      {/* <header className={`${getSeason()} h-48 flex items-center justify-center text-lg font-sans text-white`}> */}
+      <header className={`bg-green-400 h-48 flex items-center justify-center text-lg font-sans text-white`}>
         <div className="w-96 text-center pt-12 md:pt-0">{message}</div>
         <div className="absolute top-0 left-0">
-          <Counter changeSeason={changeSeason} />
+          {/* <Counter changeSeason={changeSeason} /> */}
         </div>
       </header>
       <TreePainter messageChange={(e) => handleMessageChange(e)}/>
       <button onClick={logSeason}>Log Season</button>
-      {/* <button onClick={winter}>winter</button>
-      <button onClick={fall}>fall</button> */}
+      <button onClick={winter}>winter</button>
+      <button onClick={fall}>tick</button>
     </div>
   )
 }
