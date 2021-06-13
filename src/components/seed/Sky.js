20c0eba6a292699b7d0225a11abb7eac58c43abc
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-13 13:17:04 -0500

    [sm] got sky animation for day night cycle san sunset

--- src/components/seed/Sky.js
+++ src/components/seed/Sky.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Sunset from './Sunset'
+import { useSelector } from 'react-redux'
+import { selectHour } from '../../redux/hourSlice'
+const Sky = () => {
+    const [sun, setSun] = useState({transform: "translateY(-100px)"})
+    const [sky, setSky] = useState({opacity: 0})
+    let hour = useSelector(selectHour);
 
-const Sky = ({ hour }) => {
+    useEffect(() => {
+        console.log("checking hour", hour);
+       setSun(hour <= 6 || hour >= 20 ?  {transform: "translateY(200px)"} : {transform: "translateY(-100px)"});
+       setSky(hour <= 6 || hour >= 20 ?  {opacity: 1.0} : {opacity: 0});
+    },[hour])
+
     const clickSky = () => {
         console.log("Sky");
     }
@@ -9,7 +20,14 @@ const Sky = ({ hour }) => {
     return (
         <button 
             onClick={clickSky}
-            className="absolute z-0 mx-auto lg:mr-auto h-full bg-blue-400 absolute w-full relative focus:outline-none">
+            className="z-0 mx-auto lg:mr-auto h-full bg-blue-400 relative absolute w-full z-20 overflow-hidden focus:outline-none">
+            {/* <div className="absolute w-screen transition" style={ hour <== 6 && hour >== 16 ? {transform: "translateY(200px)"} : {transform: "translateY(0px)"}}> */}
+            <div className="absolute w-screen transition duration-5000" style={sun}>
+                <div className="w-20 h-20 bg-yellow-200 shadow-glow rounded-full mx-auto">
+
+                </div>
+            </div>
+            <div className="bg-blue-900 w-full h-full transition duration-5000 z-5" style={sky}></div>
             <Sunset opacity={.25} />
         </button> 
     )   
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,14 @@ module.exports = {
         'acorn-top': '#ECA76D',
         'acorn-bottom': '#945E42',
         'water-blue' : '#75b4e3'
+      },
+      transitionDuration: {
+        '0': '0ms',
+        '2000': '2000ms',
+        '5000': '5000ms'
+      },
+      boxShadow: {
+        glow: '0 0 25px 5px rgba(255, 255, 255, 0.45)',
       }
     },
   },
