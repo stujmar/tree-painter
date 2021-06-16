44bd886979e18667f99131621807b1aa430e0406
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-15 21:09:07 -0500

    [sm] refixed the canopy offset and may have fixed the glitch on planting

--- src/components/gameBoard/Canopy.js
+++ src/components/gameBoard/Canopy.js
@@ -3,15 +3,15 @@ import React from 'react';
 const Canopy = ({age}) => {
     switch(age) {
         case 0:
-            return "";
+            return <></>;
         case 1:
-            return <div className={`rounded-full -ml-2 h-6 w-6 bg-green-300`}></div>
+            return <div className={`rounded-full -ml-2 -mb-1 h-6 w-6 bg-green-300`}></div>
         case 2:
-            return <div className={`rounded-full -ml-2 h-7 w-7 bg-green-300`}></div>
+            return <div className={`rounded-full -ml-2 -mb-1 h-7 w-7 bg-green-300`}></div>
         case 3:
-            return <div className={`rounded-full -ml-2 h-8 w-8 bg-green-300`}></div>
+            return <div className={`rounded-full -ml-2 -mb-1 h-8 w-8 bg-green-300`}></div>
         default:
-            return <div className={`rounded-full -ml-2 h-9 w-9 bg-green-300`}></div>
+            return <div className={`rounded-full -ml-2 -mb-1 h-9 w-9 bg-green-300`}></div>
     }
 }
 
