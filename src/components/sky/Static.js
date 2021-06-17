6bc4f19a578c8d29f9027bbd11445b91a5076c9f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-16 19:50:59 -0500

    [sm] got rid of static animation that slowed everything down and put just a static sky

--- src/components/sky/Static.js
+++ src/components/sky/Static.js
@@ -1,33 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const Static = () => {
-    const [ grid, setGrid ] = useState(false);
-    let skyDimensions = document.getElementById('static');
+    // const [ grid, setGrid ] = useState(false);
+    // let skyDimensions = document.getElementById('static');
     
-    if (!!skyDimensions && !grid) {
-        let array = []
-        for ( let i = 0; i < skyDimensions.clientWidth; i++ ) {
-        // for ( let i = 0; i < (Math.floor(skyDimensions.clientWidth/10) * Math.floor(skyDimensions.clientHeight/10)); i++ ) {
-        array.push({id: i})
-        }
-        // console.log(skyDimensions.clientWidth);
-        setGrid(array.map((square) => {
-            Math.floor(Math.random() * 2)
-            return <div className={`h-3 w-3 transition-colors ${Math.floor(Math.random() * 2) === 0 ? "bg-black" : "bg-white"}`} key={square.id}></div>
-        }))
-        setTimeout(() => {
-            setGrid(false)
-        },2000)
-    }
+    // if (!!skyDimensions && !grid) {
+    //     let array = []
+    //     for ( let i = 0; i < skyDimensions.clientWidth; i++ ) {
+    //     // for ( let i = 0; i < (Math.floor(skyDimensions.clientWidth/10) * Math.floor(skyDimensions.clientHeight/10)); i++ ) {
+    //     array.push({id: i})
+    //     }
+    //     setGrid(array.map((square) => {
+    //         Math.floor(Math.random() * 2)
+    //         return <div className={`h-3 w-3 transition-colors ${Math.floor(Math.random() * 2) === 0 ? "bg-black" : "bg-white"}`} key={square.id}></div>
+    //     }))
+    //     setTimeout(() => {
+    //         setGrid(false)
+    //     },2000)
+    // }
 
     return (
-    <div id="static" className="top-0 h-full w-full absolute z-100 flex flex-wrap justify-center bg-black transition-colors">
+    <div id="static" className="top-0 h-full w-full absolute flex z-30 flex-wrap justify-center bg-blue-900 transition-colors">
         <div style={{position: "absolute", left: "50%"}}>
-            <div className="font-bold text-red-500 top-6 text-4xl" style={{position: "relative", left: "-50%"}}>
+            <div className="font-bold text-orange-600 top-6 z-30 text-4xl" style={{position: "relative", left: "-50%"}}>
             PAUSED
             </div>
         </div>
-        {grid}
+        {/* {grid} */}
     </div>
     )
 };
