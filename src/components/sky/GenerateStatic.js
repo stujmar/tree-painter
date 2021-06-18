9c0b585911c3f4b36eb8e60331883ee9543c7152
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-17 20:33:35 -0500

    [sm] deleted out old static method and got new one working a little better

--- src/components/sky/GenerateStatic.js
+++ src/components/sky/GenerateStatic.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { coinFlip } from '../../utils/coinFlip';
 
-const GenerateStatic = () => {
+const GenerateStatic = ({ isPaused }) => {
 
         const [row, setRow] = useState([]);
 
@@ -9,9 +9,9 @@ const GenerateStatic = () => {
             console.log('onload');
             let array = []
             let parentDimensions = document.getElementById('static');
-            for (let i = 0; i < Math.floor(parentDimensions.clientWidth/8); i++) {
+            for (let i = 0; i < (Math.floor(parentDimensions.clientWidth/8) * Math.floor(parentDimensions.clientHeight/8)); i++) {
                 // setRow([...row, <div key={i} className="bg-white h-2 w-2"></div>])
-                array.push(<div key={i} className="bg-white h-2 w-2"></div>);
+                array.push(<div key={i} className={`${coinFlip() ? 'bg-black' : 'bg-white'} h-2 w-2`}></div>);
             }
             setRow(array);
             // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -37,13 +37,32 @@ const GenerateStatic = () => {
             }, [delay]);
         }
         useInterval(() => {
-            // coinFlip() ? console.log('tick') : console.log('tock');
+            let array = []
+            let parentDimensions = document.getElementById('static');
+            for (let i = 0; i < (Math.floor(parentDimensions.clientWidth/8) * Math.floor(parentDimensions.clientHeight/7)); i++) {
+                // setRow([...row, <div key={i} className="bg-white h-2 w-2"></div>])
+                array.push(<div key={i} className={`${coinFlip() ? 'bg-black' : 'bg-white'} h-2 w-2`}></div>);
+            }
+            setRow(array);
 
-        }, 500);
+        }, 200);
     return (
-        <div id="static" className="h-full bg-black w-full border border-red-600 z-50 absolute justify-center top-0 flex flex-wrap">
+        <>
+        
+        <div id="static" className="h-full w-full z-50 absolute justify-center top-0">
+
+            {isPaused ? <div className="relative flex flex-wrap items-start bg-black justify-center overflow-hidden">
+            <div style={{position: "absolute", left: "50%"}}>
+                <div className="font-bold text-red-600 top-4 text-blend-difference z-30 text-6xl" style={{position: "relative", left: "-50%", textStroke: "2px white"}}>
+                    PAUSED
+                </div>
+            </div>
             {row}
-        </div>
+            </div> : <></>}
+
+        </div> 
+
+        </>
     )
 }
 
