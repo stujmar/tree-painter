439a76760ecb41cca78d699edb8cb8299daa95c8
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-17 19:31:24 -0500

    [sm] GenerateStatic with useInterval is working beautifully just logging its refresh time but not generating static ... yet

--- src/components/sky/GenerateStatic.js
+++ src/components/sky/GenerateStatic.js
@@ -1,17 +1,6 @@
 import { useEffect, useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectSpeed } from '../../redux/clockSlice';
-import { selectDay, resetDay, incrementDay } from '../../redux/daySlice';
-import { selectHour, resetHour, incrementHour } from '../../redux/hourSlice';
 
 const GenerateStatic = () => {
-        // let [count, setCount] = useState(Math.floor(Math.random() * (366 - 1) + 1));
-        const dispatch = useDispatch();
-        let speed = useSelector(selectSpeed);
-        let hour = useSelector(selectHour);
-        let day = useSelector(selectDay);
-
-        // let [day, setDay] = useState(Math.floor(Math.random() * (366 - 1) + 1));
 
         function useInterval(callback, delay) {
             const savedCallback = useRef();
@@ -33,20 +22,8 @@ const GenerateStatic = () => {
             }, [delay]);
         }
         useInterval(() => {
-            if (hour < 24) {  // If the time is less than 24 tick forward an hour.
-                dispatch(incrementHour());
-            } else { // Else we need to reset to 1:00am.
-                dispatch(resetHour());
-            }
-            if (hour === 23) { // If we are about to tick over to 12:00am progress the day.
-                if (day < 365) { // Most days of the year we just tick forward one day.
-                    dispatch(incrementDay());
-                } else { // Except on New Year's Eve resest to the first day of the year.
-                    dispatch(resetDay()); 
-                }
-            }
-    
-        }, speed); // How many milliseconds it takes for an hour to pass in game.
+            console.log('tlock');
+        }, 500);
     return (<></>)
 }
 
