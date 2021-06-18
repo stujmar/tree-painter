883a19d56b1f3103065474fece622131c74548a3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-17 19:56:24 -0500

    [sm] got a row of pixels looking pretty good

--- src/components/sky/GenerateStatic.js
+++ src/components/sky/GenerateStatic.js
@@ -6,7 +6,15 @@ const GenerateStatic = () => {
         const [row, setRow] = useState([]);
 
         useEffect(() => {
-            setRow(<div className="bg-white h-2 w-2"></div>)
+            console.log('onload');
+            let array = []
+            let parentDimensions = document.getElementById('static');
+            for (let i = 0; i < Math.floor(parentDimensions.clientWidth/8); i++) {
+                // setRow([...row, <div key={i} className="bg-white h-2 w-2"></div>])
+                array.push(<div key={i} className="bg-white h-2 w-2"></div>);
+            }
+            setRow(array);
+            // eslint-disable-next-line react-hooks/exhaustive-deps
         },[])
 
         function useInterval(callback, delay) {
@@ -29,10 +37,11 @@ const GenerateStatic = () => {
             }, [delay]);
         }
         useInterval(() => {
-            coinFlip() ? console.log('tick') : console.log('tock');
+            // coinFlip() ? console.log('tick') : console.log('tock');
+
         }, 500);
     return (
-        <div id="static" className="h-full w-full border border-red-600 z-50 absolute top-0">
+        <div id="static" className="h-full bg-black w-full border border-red-600 z-50 absolute justify-center top-0 flex flex-wrap">
             {row}
         </div>
     )
