38b82c3211fe440e4dc6fe25a68c07975ee1adfa
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-06-20 13:46:57 -0500

    [sm] thickened up the bottom border on the debug drop down

--- src/components/gameBoard/Game.js
+++ src/components/gameBoard/Game.js
@@ -169,7 +169,7 @@ const Game = ( { messageChange, toggleGraph } ) => {
                     </div>
             <div 
                 style={{ transform: debugModal ? "translateY(0px)" : "translateY(-300px)" }}
-                className={`w-screen absolute top-0 ${getSeason(day).light} transition border-b-2 ${getSeason(day).border} p-2 border-box overflow-y-auto h-56`}> {/* DeBug */}
+                className={`w-screen absolute top-0 ${getSeason(day).light} transition border-b-4 ${getSeason(day).border} p-2 border-box overflow-y-auto h-56`}> {/* DeBug */}
                 <div className="flex justify-between mx-4">
                     <div>{grass ? `x: ${mouse.x}/${grass.clientWidth} y: ${mouse.y}/${grass.clientHeight}` : ""}</div>
                     <div>{grass ? `x: ${(mouse.x/grass.clientWidth* 100).toFixed()}% y: ${(mouse.y/grass.clientHeight * 100).toFixed()}%` : ""}</div>
