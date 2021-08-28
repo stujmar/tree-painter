d18502e380eaa659fe509c873baa1acb8b9a58c3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-08-28 15:42:49 -0500

    [sm] had copilot write the ratio coin flip function and I think it works

--- src/utils/coinFlip.js
+++ src/utils/coinFlip.js
@@ -1,3 +1,9 @@
 export function coinFlip() {
     return Math.floor(Math.random() * 2) === 0;
 }
+
+// a function that returns boolean depending on a ratio bewteen 0 and 1
+export function coinFlipRatio(ratio) {
+    // This is copilot code, double check it. and get it returning the correct value.
+    return Math.random() < ratio;
+}
