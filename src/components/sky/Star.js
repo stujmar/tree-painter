b3454fc0b821c67298b5a4c477aa0832dce8a2f1
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-05 19:40:57 -0500

    [sm] possibly got logic for starts to fade out during the daytime into the star component

--- src/components/sky/Star.js
+++ src/components/sky/Star.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { selectHour } from '../../redux/hourSlice';
+import { useSelector } from 'react-redux';
 
 const Star = ({starData}) => {
+    const [opacity, setOpacity] = useState(1);
+    let hour = useSelector(selectHour);
+
+    useEffect(() => {
+        setOpacity(hour <= 6 || hour >= 20 ?  1 : 0);
+     },[hour])
+
     return (
         <div 
             key={`${starData.x} + ${starData.y}`} 
@@ -8,6 +17,7 @@ const Star = ({starData}) => {
             style={{
                 left: `${starData.x}%`, 
                 top: `${starData.y}%`, 
+                opacity: opacity,
             }}
             ></div>
     );
