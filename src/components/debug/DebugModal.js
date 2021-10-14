e2cf5ff4d8109c1bb7b165c31fe1226f1c3505f2
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-13 21:43:02 -0500

    [sm] added print tree data button to debug modal, started to create component for debug model

--- src/components/debug/DebugModal.js
+++ src/components/debug/DebugModal.js
@@ -1,7 +1,20 @@
 import React from 'React';
 
 const DebugModal = () => {
-    return (<></>)
+    return (<>
+
+        <div 
+            style={{ transform: debugModal ? "translateY(0px)" : "translateY(-300px)" }}
+            className={`w-screen absolute top-0 ${getSeason(day).light} transition border-b-4 ${getSeason(day).border} p-2 border-box overflow-y-auto h-56`}> {/* DeBug */}
+            <div className="flex justify-between mx-4">
+                <div>{grass ? `x: ${mouse.x}/${grass.clientWidth} y: ${mouse.y}/${grass.clientHeight}` : ""}</div>
+                <button class="bg-green-200 px-1 rounded border shadow">Print Tree Data</button>
+                <div>{grass ? `x: ${(mouse.x/grass.clientWidth* 100).toFixed()}% y: ${(mouse.y/grass.clientHeight * 100).toFixed()}%` : ""}</div>
+            </div>
+            <Debug infoPanel={infoPanel} />
+        </div>
+
+    </>)
 }
 
 export default DebugModal;
