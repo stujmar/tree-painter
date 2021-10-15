377a755bb2b195d3324f44e6d657c331f5dad79b
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-14 21:34:39 -0500

    [sm] fixed issue with Print Tree Data button jumping around

--- src/components/gameBoard/Game.js
+++ src/components/gameBoard/Game.js
@@ -183,9 +183,9 @@ const Game = ( { messageChange, toggleGraph } ) => {
                 style={{ transform: debugModal ? "translateY(0px)" : "translateY(-300px)" }}
                 className={`w-screen absolute top-0 ${getSeason(day).light} transition border-b-4 ${getSeason(day).border} p-2 border-box overflow-y-auto h-56`}> {/* DeBug */}
                 <div className="flex justify-between mx-4">
-                    <div>{grass ? `x: ${mouse.x}/${grass.clientWidth} y: ${mouse.y}/${grass.clientHeight}` : ""}</div>
+                    <div class="w-48">{grass ? `x: ${mouse.x}/${grass.clientWidth} y: ${mouse.y}/${grass.clientHeight}` : ""}</div>
                     <button class="bg-green-200 px-1 rounded border shadow">Print Tree Data</button>
-                    <div>{grass ? `x: ${(mouse.x/grass.clientWidth* 100).toFixed()}% y: ${(mouse.y/grass.clientHeight * 100).toFixed()}%` : ""}</div>
+                    <div class="w-32 text-left">{grass ? `x: ${(mouse.x/grass.clientWidth* 100).toFixed()}% y: ${(mouse.y/grass.clientHeight * 100).toFixed()}%` : ""}</div>
                 </div>
                 <Debug infoPanel={infoPanel} />
             </div>
