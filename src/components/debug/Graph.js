829c5d7d64ac9f3f153806e87ce3cd23b852b8b7
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-25 22:13:15 -0500

    [sm] added back in the close button without breaking anything

--- src/components/debug/Graph.js
+++ src/components/debug/Graph.js
@@ -18,7 +18,13 @@ const Graph = ({toggleGraph}) => {
         // </div>
         <div className="absolute inset-0 bg-red-500 w-full h-screen z-50">
             <div className="relative w-full h-full bg-gray-200">
-
+                <button
+                    className="absolute bg-gray-300 hover:bg-gray-200 right-2 top-2 rounded-md z-50 opacity-100"
+                    onClick={toggleGraph}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button> 
             </div>
         </div>
     )
