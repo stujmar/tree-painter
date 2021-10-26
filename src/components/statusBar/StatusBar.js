4f34361d8e93ef01d5ae12107c73b7b51d291fbc
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-25 20:54:23 -0500

    [sm] have acorn icon trigger a function call in the status bar

--- src/components/statusBar/StatusBar.js
+++ src/components/statusBar/StatusBar.js
@@ -11,11 +11,15 @@ const StatusBar = () => {
     let day = useSelector(selectDay);
     let { seeds, water, stars} = useSelector(selectResources);
 
+    const addAcorn = () => {
+        console.log("Acorn Added");
+    }
+
     return (
         <div className={`w-full comfortaa ${getSeason(day).dark}`}>
             {/* <div className="grid grid-cols-3 max-w-7xl mx-auto justify-start items-top py-1 w-max"> */}
             <div className="flex pl-3 mx-auto justify-start items-top py-1 justify-start gap-3">
-                <button class="focus:outline-none"><AcornIcon amount={seeds} color={getSeason(day).text} /></button>
+                <button className="focus:outline-none" onClick={() => addAcorn()}><AcornIcon amount={seeds} color={getSeason(day).text} /></button>
                 <WaterIcon amount={water} color={getSeason(day).text}/>
                 <StarIcon amount={stars} color={getSeason(day).text}/>
             </div>
