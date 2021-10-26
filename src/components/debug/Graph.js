6b2a684b69c9fa8ce48f587060b07c83315d9077
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-25 22:11:06 -0500

    [sm] got graph pop up to cover screen and not add height

--- src/components/debug/Graph.js
+++ src/components/debug/Graph.js
@@ -2,17 +2,24 @@ import React from 'react';
 
 const Graph = ({toggleGraph}) => {
     return (
-        <div className="">
-            <button
-                className="absolute bg-green-100 hover:bg-green-200 right-2 top-2 rounded-md z-50 opacity-100"
-                onClick={toggleGraph}
-            >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-green-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button> 
-            <div className="fixed absolute h-screen z-20 w-screen bg-green-600 opacity-50">
-        </div>
+        // <div className="absolute">
+        //     <button
+        //         className="absolute bg-green-100 hover:bg-green-200 right-2 top-2 rounded-md z-50 opacity-100"
+        //         onClick={toggleGraph}
+        //     >
+        //         <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-green-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        //             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+        //         </svg>
+        //     </button> 
+        
+        //     <div className="fixed h-48 z-20 w-screen bg-green-600 opacity-80">
+        // </div>
+        //     <div className="w-24 h-24 border bg-white"></div>
+        // </div>
+        <div className="absolute inset-0 bg-red-500 w-full h-screen z-50">
+            <div className="relative w-full h-full bg-gray-200">
+
+            </div>
         </div>
     )
 }
