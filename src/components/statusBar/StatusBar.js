e65daa16143bef002e015bbf90223415ebdd0c05
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-25 20:52:03 -0500

    [sm] added a button wrapping around the acorn icon as a hidden way to test more trees

--- src/components/statusBar/StatusBar.js
+++ src/components/statusBar/StatusBar.js
@@ -10,11 +10,12 @@ import {getSeason} from '../../utils/getSeason';
 const StatusBar = () => {
     let day = useSelector(selectDay);
     let { seeds, water, stars} = useSelector(selectResources);
+
     return (
         <div className={`w-full comfortaa ${getSeason(day).dark}`}>
             {/* <div className="grid grid-cols-3 max-w-7xl mx-auto justify-start items-top py-1 w-max"> */}
             <div className="flex pl-3 mx-auto justify-start items-top py-1 justify-start gap-3">
-                <AcornIcon amount={seeds} color={getSeason(day).text} />
+                <button class="focus:outline-none"><AcornIcon amount={seeds} color={getSeason(day).text} /></button>
                 <WaterIcon amount={water} color={getSeason(day).text}/>
                 <StarIcon amount={stars} color={getSeason(day).text}/>
             </div>
