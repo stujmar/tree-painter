059a18809d30e18ba3620738b0439029eb4aaea1
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-28 22:42:04 -0500

    [sm] made function to return a percentage based on passed values

--- src/components/debug/Graph.js
+++ src/components/debug/Graph.js
@@ -27,6 +27,11 @@ const Graph = () => {
         getVerticalLines(4);
     },[])
 
+    function getPercentage(current, total) {
+        let value = (total / current) * 100;
+        return `${value}%`;
+    }
+
     return (
         <div className="border-2 border-black w-96 h-96 bg-white mx-auto text-center relative">
             <div className="h-2 w-2 rounded-full bg-red-500 absolute" style={{top: '0%', left: '0%'}}></div>
