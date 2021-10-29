1771c0176d62725d32c19db7e0876de7af09acc7
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-28 21:58:27 -0500

    [sm] import dispatch into graph component

--- src/components/debug/Graph.js
+++ src/components/debug/Graph.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
 
 const Graph = () => {
+    const dispatch = useDispatch();
 
     return (
         <div className="border-2 border-black w-48 h-48 bg-white mx-auto text-center pt-12">
