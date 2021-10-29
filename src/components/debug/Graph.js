f0c131e772447b367d92e65d35abe11029cf6026
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-28 22:05:25 -0500

    [sm] got red dot to show up in graphing area

--- src/components/debug/Graph.js
+++ src/components/debug/Graph.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 const Graph = () => {
     const dispatch = useDispatch();
 
     return (
-        <div className="border-2 border-black w-48 h-48 bg-white mx-auto text-center pt-12">
-            <h1>Graphs Go Here</h1>
+        <div className="border-2 border-black w-96 h-96 bg-white mx-auto text-center relative">
+            <div className="h-2 w-2 rounded-full bg-red-500 absolute" style={{top: '0%', left: '0%'}}></div>
+            <h1 className="mt-12">Graphs Go Here</h1>
         </div>
     );
 }
