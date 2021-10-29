2361cae8b7eb585d18823b78e7da20cecc39c3ab
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-28 23:02:30 -0500

    [sm] kind of got the lines to work with some shakey logic

--- src/components/debug/Graph.js
+++ src/components/debug/Graph.js
@@ -10,33 +10,40 @@ const Graph = () => {
     const getHorizontalLines = (density) => {
         console.log("getting horizontal lines")
         setHorizontalLines([...Array(density)].map((elementInArray, index) => {
-            return <div key={index}>{`h: ${index}`}</div>
+            return <div key={index} className="border-t-2 border-green-500 absolute w-full">{`h: ${getPercentage(index, density)}`}</div>
         }));
     };
 
     const getVerticalLines = (density) => {
-        setVerticalLines([...Array(density)].map((elementInArray, index) => {
-            return <div key={index}>{`v: ${getPercentage(index, density)}`}</div>
+        setVerticalLines([...Array(density)].filter((e, i) => i !== 0).map((elementInArray, index) => {
+            return <div 
+                key={index} 
+                style={{left: getPercentage(index + 1, density)}}
+                className="border-r-2 border-red-500 absolute h-full"
+                ></div>
         }));
-        console.log("getting vertical lines")
+        console.log(horizontalLines)
     };
 
     useEffect(() => {
         console.log("Graph component mounted");
         getHorizontalLines(4);
-        getVerticalLines(4);
+        getVerticalLines(8);
     },[])
 
     function getPercentage(current, total) {
-        let value = current === 0 ? 0 : ((current + 1) / total);
-        console.log(total, current + 1, value);
-        return `${value}%`;
+        let value = current === 0 ? 0 : ((current) / total);
+        console.log(total, current, `${value * 100}%`);
+        if (value === 0) {
+            return '0%';
+        }
+        return `${value * 100}%`;
     }
 
     return (
         <div className="border-2 border-black w-96 h-96 bg-white mx-auto text-center relative">
             <div className="h-2 w-2 rounded-full bg-red-500 absolute" style={{top: '0%', left: '0%'}}></div>
-            <h1 className="mt-12">Graphs Go Here</h1>
+            {/* <h1 className="mt-12">Graphs Go Here</h1> */}
             {verticalLines}
             {horizontalLines}
         </div>
