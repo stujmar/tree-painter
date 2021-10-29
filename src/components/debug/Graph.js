7dd565c628dd1e5a538ed7b1bf6ab50099780179
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-28 22:24:32 -0500

    [sm] got function to return a set of x divs into the graph

--- src/components/debug/Graph.js
+++ src/components/debug/Graph.js
@@ -1,28 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 
 const Graph = () => {
     const dispatch = useDispatch();
-
-    const getHorizontalLines = () => {
+    const [verticalLines, setVerticalLines] = useState([]);
+    const getHorizontalLines = (density) => {
         console.log("getting horizontal lines")
     };
 
-    const getVerticalLines = () => {
+    const getVerticalLines = (density) => {
+        setVerticalLines([...Array(density)].map((elementInArray, index) => {
+            return <div key={index}>{`index: ${index}`}</div>
+        }));
         console.log("getting vertical lines")
     };
 
     useEffect(() => {
         console.log("Graph component mounted");
-        getHorizontalLines();
-        getVerticalLines();
+        getHorizontalLines(4);
+        getVerticalLines(4);
     },[])
 
     return (
         <div className="border-2 border-black w-96 h-96 bg-white mx-auto text-center relative">
             <div className="h-2 w-2 rounded-full bg-red-500 absolute" style={{top: '0%', left: '0%'}}></div>
             <h1 className="mt-12">Graphs Go Here</h1>
+            {verticalLines}
         </div>
     );
 }
