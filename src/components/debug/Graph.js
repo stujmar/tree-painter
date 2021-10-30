0a6e24c9ff3d32c2810e0db0fbcaf3ed8de87649
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-29 20:06:16 -0500

    [sm] bound inputs to state for grid count

--- src/components/debug/Graph.js
+++ src/components/debug/Graph.js
@@ -5,6 +5,8 @@ const Graph = () => {
   const dispatch = useDispatch();
   const [verticalLines, setVerticalLines] = useState([]);
   const [horizontalLines, setHorizontalLines] = useState([]);
+  const [xCount, setXCount] = useState(10);
+  const [yCount, setYCount] = useState(10);
 
   const getHorizontalLines = (density) => {
     console.log("getting horizontal lines")
@@ -29,9 +31,10 @@ const Graph = () => {
 
   useEffect(() => {
     console.log("Graph component mounted");
-    getHorizontalLines(4);
-    getVerticalLines(8);
-  },[])
+    getHorizontalLines(yCount);
+    getVerticalLines(xCount);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[xCount, yCount]);
 
   function getPercentage(current, total) {
     let value = current === 0 ? 0 : ((current) / total);
@@ -42,6 +45,16 @@ const Graph = () => {
     return `${value * 100}%`;
   }
 
+  function handleInputs(e) {
+    const { name, value } = e.target;
+    if (name === 'x') {
+      setXCount(value);
+    } else if (name === 'y') {
+      setYCount(value);
+    }
+    console.log(name, value);
+  }
+
   return (
     <div>
       <div className="border-2 border-black w-96 h-96 bg-white mx-auto text-center relative">
@@ -49,6 +62,10 @@ const Graph = () => {
         {/* <h1 className="mt-12">Graphs Go Here</h1> */}
         {verticalLines}
         {horizontalLines}
+      </div>
+      <div className="w-max mx-auto mt-4">
+        X<input type="number" name="x" value={xCount} onChange={(e) => handleInputs(e)}/>
+        Y<input type="number" name="y" value={yCount} onChange={(e) => handleInputs(e)}/>
       </div>
     </div>
   );
