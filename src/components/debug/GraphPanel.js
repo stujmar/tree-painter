5e6fdc0643bcd2d5cf31fbb1dc887fac6bbfe360
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-10-29 20:24:10 -0500

    [sm] restyled exit button to graph panel

--- src/components/debug/GraphPanel.js
+++ src/components/debug/GraphPanel.js
@@ -20,9 +20,9 @@ const GraphPanel = ({toggleGraph}) => {
         <div className="absolute inset-0 bg-red-500 w-full h-screen z-50">
             <div className="relative w-full h-full pt-48" style={{ background: "linear-gradient(0deg, rgba(75,182,204,1) 0%, rgba(41,103,120,1) 100%)" }}>
                 <button
-                    className="absolute bg-gray-300 hover:bg-gray-200 right-2 top-2 rounded-md z-50 opacity-100"
+                    className="absolute bg-amber-100 hover:bg-amber-200 right-2 top-2 rounded-full z-50 opacity-100"
                     onClick={toggleGraph}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-gray-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-amber-800" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button> 
