fed797b28f34fb16ddc522c4f100cd24d5b12de5
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-04 21:05:37 -0500

    [sm] did hacky trick to get canpy back on top

--- src/components/gameBoard/Canopy.js
+++ src/components/gameBoard/Canopy.js
@@ -5,13 +5,13 @@ const Canopy = ({age}) => {
         case 0:
             return <></>;
         case 1:
-            return <div className={`rounded-full -ml-2 -mb-1 h-6 w-6 bg-green-300`}></div>
+            return <div style={{zIndex: 50}} className={`rounded-full -ml-2 -mb-1 h-6 w-6 bg-green-300`}></div>
         case 2:
-            return <div className={`rounded-full -ml-2 -mb-1 h-7 w-7 bg-green-300`}></div>
+            return <div style={{zIndex: 50}} className={`rounded-full -ml-2 -mb-1 h-7 w-7 bg-green-300`}></div>
         case 3:
-            return <div className={`rounded-full -ml-2 -mb-1 h-8 w-8 bg-green-300`}></div>
+            return <div style={{zIndex: 50}} className={`rounded-full -ml-2 -mb-1 h-8 w-8 bg-green-300`}></div>
         default:
-            return <div className={`rounded-full -ml-2 -mb-1 h-9 w-9 bg-green-300`}></div>
+            return <div style={{zIndex: 50}} className={`rounded-full -ml-2 -mb-1 h-9 w-9 bg-green-300`}></div>
     }
 }
 
--- src/components/gameBoard/Trunk.js
+++ src/components/gameBoard/Trunk.js
@@ -7,9 +7,9 @@ const Trunk = ({trunkData}) => {
 
     return (
         <div className="w-10 h-2 -ml-2">
-            <div className="w-4 h-2 bg-orange-400 mx-auto opacity-100 text-green-100">
-              {trunkData.left ? <p class="absolute -left-2">{trunkData.left}</p> : null}
-              {trunkData.right ? <p class="absolute right-0">{trunkData.right}</p> : null}
+            <div className="w-4 h-2 bg-orange-400 mx-auto opacity-100 text-green-100 relative">
+              {trunkData.left ? <div style={{width: trunkData.left, zIndex: 0}}className="h-1 bg-orange-400 absolute right-4"></div> : null}
+              {trunkData.right ? <p className="absolute right-0"></p> : null}
             </div>
 
         </div>
