045b3692cbf3c4d9455ca18bf97b8690ba921265
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-04 21:06:47 -0500

    [sm] got left and right branches growing

--- src/components/gameBoard/Trunk.js
+++ src/components/gameBoard/Trunk.js
@@ -9,7 +9,7 @@ const Trunk = ({trunkData}) => {
         <div className="w-10 h-2 -ml-2">
             <div className="w-4 h-2 bg-orange-400 mx-auto opacity-100 text-green-100 relative">
               {trunkData.left ? <div style={{width: trunkData.left, zIndex: 0}}className="h-1 bg-orange-400 absolute right-4"></div> : null}
-              {trunkData.right ? <p className="absolute right-0"></p> : null}
+              {trunkData.right ? <div style={{width: trunkData.right, zIndex: 0}}className="h-1 bg-orange-400 absolute left-4"></div> : null}
             </div>
 
         </div>
