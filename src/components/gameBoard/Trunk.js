8ed565eabd4a321da70411eb6bad267b612e188c
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-09 22:23:59 -0600

    [sm] got branches looking a little less broken

--- src/components/gameBoard/Trunk.js
+++ src/components/gameBoard/Trunk.js
@@ -16,13 +16,13 @@ const Trunk = ({trunkData, color, girth}) => {
             <div style={{width: girth}} className="h-2 bg-orange-400 opacity-100 text-green-100 relative">
               {trunkData.left ? <div style={{width: trunkData.left*3, zIndex: 0,  transform: "rotate(20deg)", right: girth - 2 }}className="h-1 absolute top-1">
                   <div className="relative w-full h-full">
-                    <div style={{height:  returnBranchWidth(trunkData.left), width: returnBranchWidth(trunkData.left), top: -trunkData.left}} className={`h-3 w-3 ${color} absolute rounded-full -left-2`}></div>
+                    <div style={{height:  returnBranchWidth(trunkData.left), width: returnBranchWidth(trunkData.left), top: -trunkData.left/2 -3}} className={`h-3 w-3 ${color} absolute rounded-full -left-2`}></div>
                   </div>
                   <div className="absolute inset-0 w-full h-full bg-orange-400"></div>
               </div> : null}
               {trunkData.right ? <div style={{width: trunkData.right*3, zIndex: 0, transform: "rotate(-20deg)", left: girth - 2}}className="h-1 absolute top-1">
                 <div className="relative w-full h-full">
-                    <div style={{height: returnBranchWidth(trunkData.right), width: returnBranchWidth(trunkData.right), top: -trunkData.right}} className={`h-3 w-3 ${color} absolute rounded-full -right-2`}></div>
+                    <div style={{height: returnBranchWidth(trunkData.right), width: returnBranchWidth(trunkData.right), top: -trunkData.right/2 - 3}} className={`h-3 w-3 ${color} absolute rounded-full -right-2`}></div>
                   </div>
                   <div className="absolute inset-0 w-full h-full bg-orange-400"></div>
               </div> : null}
