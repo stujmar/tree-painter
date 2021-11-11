c91c09f9a9ca433d9b31beec4c1958039fef86ad
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-10 20:07:50 -0600

    [sm] was able to style mode indicator with season and center it

--- src/components/header/TimeControls.js
+++ src/components/header/TimeControls.js
@@ -26,13 +26,14 @@ const TimeControls = () => {
 
     return (    
         <div className="flex w-screen justify-between p-3 comfortaa text-lg">
+            <div className="absolute w-full -mt-4 h-14 rounded-b-lg">
+                <div className="w-max mx-auto h-full bg-white opacity-60 rounded-b-lg">
+                    <div className="p-4"><span className={`font-bold ${getSeason(day).darkText}`}>MODE INDICATOR</span></div>
+                </div>
+            </div>
             <div className="flex md:flex-col w-max items-start"> {/* LEFT SECTION */}
                 <div>{getDate(day)}</div>
                 <div className="ml-2 md:ml-0">{getTime(hour)}</div>
-            </div>
-            <div className="relative -mt-4 h-14 rounded-b-lg">
-             <div className="opacity-100 p-4">MODE INDICATOR</div>
-             <div className="absolute inset-0 bg-white opacity-70 rounded-b-lg"></div>
             </div>
             <div className="flex flex-col items-end"> {/*RIGHT SECTION*/}
                     <div className="flex items-start"> {/*SPEED CONTROLS*/}
