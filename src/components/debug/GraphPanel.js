e91d5bd9f28269146ffafc99184f5f0ac6c041f8
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-17 21:55:29 -0600

    [sm] wired up new toggle switch to work and got rid of old button

--- src/components/debug/GraphPanel.js
+++ src/components/debug/GraphPanel.js
@@ -16,11 +16,10 @@ const GraphPanel = ({toggleGraph}) => {
     return (
         <div className="container mx-auto border">
             <div className="absolute inset-0 w-full h-screen z-50">
-                <div className="relative w-full h-full pt-32 bg-green-500">
-                    <button type="button" className={`p-2 block w-max mx-auto rounded focus:outline-none ${isSandbox ? "bg-red-400" : "bg-green-200"}`} onClick={toggleSandbox}>Toggle Sandbox Mode</button>
+                <div className="relative w-full h-full pt-10 bg-green-500">
                     <div className="mx-auto flex justify-center py-4 items-center">
                         <div className="font-bold text-green-800 text-xl comfortaa mr-4">Toggle Sandbox Mode</div>
-                        <ToggleButton />
+                        <ToggleButton value={isSandbox} onClick={toggleSandbox} />
                     </div>
                     <button
                         className="fixed bg-white bg-opacity-0 hover:bg-opacity-30 focus:outline-none left-2 top-2 rounded-full z-50 opacity-100"
--- src/components/debug/ToggleButton.js
+++ src/components/debug/ToggleButton.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 
-const ToggleButton = () => {
-
-    const [isToggled, setToggled] = useState(false);
+const ToggleButton = ({ value, onClick }) => {
 
     const handleClick = () => {
-        console.log("clicking at least");
-        setToggled(!isToggled);
+        onClick();
     }
 
     return (
         <button type="button" className="focus:outline-none" onClick={handleClick}>
         <div className="w-16 h-8 bg-green-700 shadow-lg rounded-full">
             <div 
-                style={{ transform: `${isToggled ? 'translateX(0)' : 'translateX(32px)'}`,
-                background: `${isToggled ? '#E4E4E7' : '#A3E635'}` }}
+                style={{ transform: `${value ? 'translateX(0)' : 'translateX(32px)'}`,
+                background: `${value ? '#E4E4E7' : '#A3E635'}` }}
                 className="w-8 h-8 focus:outline-none ease-in-out duration-200 shadow-lg transform rounded-full"></div>
         </div>
         </button>
