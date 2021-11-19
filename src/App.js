4ff5d7c67cfd71271692d97da56c2e215f9ef886
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-18 19:32:18 -0600

    [sm] made an explainer pop up that instructs you on how to get started with modes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Game from './components/gameBoard/Game';
 import StatusBar from './components/statusBar/StatusBar';
 import GraphPanel from './components/debug/GraphPanel';
 import Header from './components/header/Header';
+import MessageModal from './components/gameBoard/MessageModal';
+import { useSelector } from 'react-redux';
+import { selectMode } from './redux/gameSlice';
 
 const App = () => {
   const [ message, setMessage ] = useState("");
   const [ graph, setGraph ] = useState(false);
+  let gameMode = useSelector(selectMode);
 
   const handleMessageChange = (payload) => {
     setMessage(payload);
@@ -20,9 +24,10 @@ const App = () => {
 
 
   return (
-    <div className="App">
+    <div className="App relative">
       {graph ? <GraphPanel toggleGraph={toggleGraph}/> : <></>}
       <ClockService />
+      {gameMode === "NO_MODE" ? <MessageModal /> : null}
       <Header message={message} />
       <StatusBar />
       <Game messageChange={(e) => handleMessageChange(e)} toggleGraph={toggleGraph} />
