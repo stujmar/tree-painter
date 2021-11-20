5a2715b1c06600c473080f5240d52ee3bd45ea5f
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-20 17:56:14 -0600

    [sm] graph panel now full height again made hover color for back button green not white

--- src/components/debug/GraphPanel.js
+++ src/components/debug/GraphPanel.js
@@ -15,13 +15,13 @@ const GraphPanel = ({toggleGraph}) => {
 
     return (
         <div className="absolute inset-0 z-50">
-                <div className="relative w-full pt-10 bg-green-400">
+                <div className="relative w-full h-full pt-10 bg-green-400">
                     <div className="mx-auto flex justify-center py-4 items-center">
                         <div className="font-bold text-green-800 text-xl comfortaa mr-4">Toggle Sandbox Mode</div>
                         <ToggleButton value={isSandbox} onClick={toggleSandbox} />
                     </div>
                     <button
-                        className="fixed bg-white bg-opacity-0 hover:bg-opacity-30 focus:outline-none left-2 top-2 rounded-full z-50 opacity-100"
+                        className="fixed hover:bg-green-500 focus:outline-none left-2 top-2 rounded-full z-50 opacity-100"
                         onClick={toggleGraph}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 fill text-white" fill="none"  viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
