dec2b32ff4bb1eb55aa0d0138fed2c4c1ff914c3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-19 18:47:04 -0600

    [sm] built a unified place for all the message center copy

---
+++ src/utils/getMessages.js
@@ -0,0 +1,6 @@
+export const getMessages = {
+    "welcome": "Click the green grass below to plant a tree.",
+    "planting": "Plant some trees!",
+    "watering": "Water your trees!",
+    "harvesting": "Harvest your trees!",
+};
