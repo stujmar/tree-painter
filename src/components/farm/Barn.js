56673b360337cb1aaf5a03a09d27b4c5eb362029
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-21 19:23:45 -0600

    [sm] got woodpile to display in the barn

--- src/components/farm/Barn.js
+++ src/components/farm/Barn.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { selectMilestones } from '../../redux/gameSlice';
+import { selectMilestones, selectResources } from '../../redux/gameSlice';
 
 const Barn = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [woodPile, setWoodPile] = useState([]);
+    let wood = useSelector(selectResources).wood;
     let isBarnUnlocked = useSelector(selectMilestones).barn;
     let barnStyle = {
         transform: isBarnUnlocked ? "translateY(0px)" : "translateY(75px)",
@@ -13,6 +15,11 @@ const Barn = () => {
         height: "72px"
     }
 
+    useEffect(() => {
+        let woodArray = Array.from({length: wood}, (v, k) => k);
+        setWoodPile(woodArray.map(_pile => <div key={_pile} className="w-3 h-1 bg-orange-500 border-t border-l-2 border-orange-700"></div>))
+    },[wood]);
+
     const handleBarnClick = () => {
         setIsOpen(!isOpen);
     }
@@ -20,7 +27,7 @@ const Barn = () => {
     return (
         <div className="absolute bottom-0 left-0 z-10">
             <button type="button" onClick={handleBarnClick} className={`block relative focus:outline-none transition duration-2000`} style={barnStyle}>  
-                <svg className={`absolute z-10 bottom-0 ${isOpen ? " hidden": " block"}`} width="150" height="72" viewBox="0 0 150 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg className={`absolute z-30 bottom-0 ${isOpen ? " hidden": " block"}`} width="150" height="72" viewBox="0 0 150 72" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M126.648 71.9848H121.559C121.559 71.9848 121.741 66.1687 125.286 65.3953C127.9 64.8261 130.124 65.6755 130.373 65.8776C130.373 65.8776 130.373 60.181 135.734 59.7943C141.095 59.4076 142.019 63.5812 142.019 63.5812C142.019 63.5812 144.226 62.2298 146.314 64.3546C148.402 66.4794 147.395 68.602 147.395 68.602C148.038 68.9533 148.601 69.4356 149.048 70.0181C149.494 70.6006 149.815 71.2705 149.989 71.9848H126.648Z" fill="#498E1C"/>
                     <path d="M83.5738 26.7838H11.8014V71.9435H83.5738V26.7838Z" fill="#C62626"/>
                     <path d="M139.494 71.9435H82.521V26.7838L92.3184 12.7988L111.007 2.06396L128.797 12.7988L139.494 26.7838V71.9435Z" fill="#C93535"/>
@@ -51,7 +58,12 @@ const Barn = () => {
                     <path d="M24.6923 71.9978H30.0731C30.0731 71.9978 29.8807 66.1796 26.1342 65.4083C22.3878 64.6371 20.7535 65.8907 20.7535 65.8907C20.7535 65.8907 20.7535 60.1941 15.0852 59.8074C9.41689 59.4207 8.43758 63.5942 8.43758 63.5942C8.43758 63.5942 6.10714 62.2429 3.89776 64.3677C1.68838 66.4925 2.74551 68.6151 2.74551 68.6151C2.74551 68.6151 0.583694 69.608 0.00216484 71.9978H24.6923Z" fill="#498E1C"/>
                     <path d="M142.038 30.0688H139.494V26.7838L142.038 30.0688Z" fill="#664922"/>
                 </svg>
-                <svg className="absolute bottom-0" width="150" height="72" viewBox="0 0 150 72" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <div className="left-6 z-20 absolute bottom-0">
+                    <div className="grid grid-cols-4 row-end-auto transform rotate-180">
+                        {woodPile}
+                    </div>
+                </div>
+                <svg className="absolute z-10 bottom-0" width="150" height="72" viewBox="0 0 150 72" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M3.10438 72C3.10438 72 3.16705 71.2548 4.03178 70.5139C4.58776 70.0578 5.24842 69.7486 5.95364 69.6145L4.69547 67.6722C4.61981 67.431 4.52685 66.7684 5.39157 65.9341C5.55607 65.755 5.75422 65.6103 5.97462 65.5085C6.19502 65.4067 6.43328 65.3498 6.67568 65.341C6.91879 65.3421 7.1592 65.3924 7.3826 65.4887L10.0589 67.2268L10.541 64.1026C10.6145 63.8723 11.0382 62.7469 12.5148 62.2298L13.9654 61.7214V27.9201H12.0197L25.1463 11.5452L42.9078 2.17261H110.473L128.088 12.5077L140.423 27.894H137.336L137.444 71.987L3.10438 72Z" fill="#B27F2D"/>
                     <path d="M147.406 68.602C147.406 68.602 148.407 66.4772 146.325 64.3546C144.243 62.232 142.029 63.5811 142.029 63.5811C141.839 62.9157 141.516 62.296 141.08 61.7597C140.644 61.2235 140.104 60.7817 139.494 60.4613V30.0666H142.04L143.796 28.6457L129.528 10.8304L111.059 -0.00436401H42.3781L23.7541 9.82231L9.48605 27.6376L11.168 29.6408H11.8165V60.1875C9.02775 61.1652 8.45488 63.5964 8.45488 63.5964C8.45488 63.5964 6.12228 62.245 3.91506 64.3676C1.70784 66.4903 2.76063 68.6172 2.76063 68.6172C2.76063 68.6172 0.598825 69.6101 0.0172958 72H30.0904C30.0904 72 30.0904 71.9782 30.0904 71.9435H62.2799C62.2799 71.9609 62.2798 71.9782 62.2647 71.9978H92.3183C92.3183 71.9978 92.3183 71.9761 92.3183 71.9435H121.566C121.566 71.9674 121.566 71.9848 121.566 71.9848H150.004C149.83 71.2701 149.508 70.6 149.061 70.0175C148.613 69.4349 148.05 68.9528 147.406 68.602Z" fill="#664922"/>
                     <path d="M3.10438 72C3.10438 72 3.16705 71.2548 4.03178 70.5139C4.58776 70.0578 5.24842 69.7486 5.95364 69.6145L4.69547 67.6722C4.61981 67.431 4.52685 66.7684 5.39157 65.9341C5.55607 65.755 5.75422 65.6103 5.97462 65.5085C6.19502 65.4067 6.43328 65.3498 6.67568 65.341C6.91879 65.3421 7.1592 65.3924 7.3826 65.4887L10.0589 67.2268L10.541 64.1026C10.6145 63.8723 11.0382 62.7469 12.5148 62.2298L13.9654 61.7214V27.9201H12.0197L25.1463 11.5452L42.9078 2.17261H110.473L128.088 12.5077L140.423 27.894H137.336L137.444 71.987L3.10438 72Z" fill="#B27F2D"/>
