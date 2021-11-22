e86159ab4d27f12e4884e23249cca27f2fec6a62
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-21 18:56:30 -0600

    [sm] got well to be behind the trees again

--- src/components/farm/Well.js
+++ src/components/farm/Well.js
@@ -23,7 +23,7 @@ const Well = () => {
         <button 
             onClick={drawWater}
             style={wellStyle}
-            className={`z-10 absolute bottom-0 right-12 z-20 focus:outline-none no-select transition duration-2000`}
+            className={`z-10 absolute bottom-0 right-12 z-19 focus:outline-none no-select transition duration-2000`}
             >
             <svg width="47" height="50" viewBox="0 0 47 50" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M36.0477 38.0697H11.7827V50H36.0477V38.0697Z" fill="#BF5C49"/>
