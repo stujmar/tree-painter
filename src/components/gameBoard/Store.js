1f7c8ece8f26cb4e79fbbfa025fa953f73934912
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-23 19:24:37 -0600

    [sm] drastically improved layout of the store with a grid of buttons

--- src/components/gameBoard/Store.js
+++ src/components/gameBoard/Store.js
@@ -3,10 +3,17 @@ import React from "react";
 const Store = ({ onClick }) => {
     return (
         <div className="absolute z-20 w-full">
-            <div className="sm:w-11/12 md:max-w-lg bg-green-100 font-bold text-green-800 comfortaa p-4 text-center mt-2 mx-2 sm:mt-16 rounded-lg text-xl sm:text-2xl shadow sm:mx-auto">
-                <span>Welcome to the treelapse store, more content coming soon</span>
-            <br></br>
-            <button className="block ml-auto w-min border-4 border-green-700 px-2 font-bold rounded mt-2" aria-label="Exit Store" onClick={onClick} >exit</button>
+            <div className="w-11/12 sm:w-10/12 md:max-w-lg bg-green-100 font-bold text-green-800 comfortaa p-4 text-center mt-2 rounded-lg text-lg sm:text-xl shadow mx-auto">
+                <span className="px-4">Click to buy buildings which unlock new game features</span>
+                <div class="w-full grid gap-2 grid-cols-2 sm:grid-cols-3 mt-3">
+                    <button className="h-20 aspect-h-1 aspect-w-1 bg-green-300 hover:bg-green-400 rounded-lg"></button>
+                    <button className="h-20 aspect-h-1 aspect-w-1 bg-green-300 hover:bg-green-400 rounded-lg"></button>
+                    <button className="h-20 aspect-h-1 aspect-w-1 bg-green-300 hover:bg-green-400 rounded-lg"></button>
+                    <button className="h-20 aspect-h-1 aspect-w-1 bg-green-300 hover:bg-green-400 rounded-lg"></button>
+                    <button className="h-20 aspect-h-1 aspect-w-1 bg-green-300 hover:bg-green-400 rounded-lg"></button>
+                    <button className="h-20 aspect-h-1 aspect-w-1 bg-green-300 hover:bg-green-400 rounded-lg"></button>
+                </div>
+            <button className="block mx-auto w-min text-base comfortaa font-bold pt-1 focus:outline-none text-green-50 bg-green-600 hover:bg-green-700 px-2 font-medium rounded mt-4" aria-label="Exit Store" onClick={onClick} >EXIT</button>
             </div>
         </div>
     );
