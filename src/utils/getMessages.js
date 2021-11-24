34b2371c93750926fb928fa7c1d6400e2526c5c8
Stuart Marsh <stujmar@gmail.com>
2021-11-24 16:08:12 -0600

    [sm] updated the water a tree method

--- src/utils/getMessages.js
+++ src/utils/getMessages.js
@@ -1,6 +1,6 @@
 export const getMessages = {
     "welcome": "Click the green grass below to plant a tree.",
     "PLANTING": "Plant some trees!",
-    "WATERING": "Water your trees!",
+    "WATERING": "Click a tree to watch it grow!",
     "HARVEST": "Harvest your trees!",
 };
