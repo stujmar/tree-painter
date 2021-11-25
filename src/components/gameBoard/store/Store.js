1370d534943575556c5e1bed81c42cb032493342
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-25 17:39:14 -0600

    [sm] made it so you can't buy an item if you can't afford it

--- src/components/gameBoard/store/Store.js
+++ src/components/gameBoard/store/Store.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {selectMilestones, setMilestone, toggleStore} from '../../../redux/gameSlice'
+import {selectMilestones, selectResources, setMilestone, toggleStore} from '../../../redux/gameSlice'
 import { 
   barnWhiteSVG,
   barnColorSVG,
@@ -13,6 +13,7 @@ import {
 const Store = () => {
 
     const dispatch = useDispatch();
+    let reasources = useSelector(selectResources);
     let isSpeedBought =  useSelector(selectMilestones).speed
     let isTractorBought =  useSelector(selectMilestones).tractor
     let isWaterBought =  useSelector(selectMilestones).water
@@ -37,17 +38,17 @@ const Store = () => {
       },
       "water": {
         name: "water",
-        price: 1,
+        price: 5,
         currency: "seeds"
       },
       "barn": {
         name: "barn",
-        price: 1,
+        price: 10,
         currency: "seeds"
       },
       "silo": {
         name: "silo",
-        price: 1,
+        price: 15,
         currency: "seeds"
       },
       "stars": {
@@ -58,7 +59,11 @@ const Store = () => {
     };
 
     const handleBuy = (item) => {
+       if ( reasources[item.currency] >= item.price ) {
         dispatch(setMilestone(item.name))
+       } else {
+         console.log("can't afford");
+       }
     }
 
     return (
@@ -66,14 +71,14 @@ const Store = () => {
           <div className="w-11/12 sm:w-10/12 md:max-w-lg bg-green-100 font-bold text-green-800 shadow-lg comfortaa p-4 text-center mt-2 md:mt-10 rounded-lg text-lg sm:text-xl shadow mx-auto">
             <span className="px-4">Click to buy buildings which unlock new game features</span>
             <div className="w-full grid gap-2 grid-cols-2 sm:grid-cols-3 mt-3">
-              <button
+              {/* <button
                 onClick={() => handleBuy(prices.speed)}
                 className={`focus:outline-none h-20 aspect-h-1 aspect-w-1 ${isSpeedBought ? "bg-green-500" : "bg-green-400 hover:bg-green-500"} rounded-lg`}><div className="flex flex-col justify-center">Unlock Speed</div>
-              </button>
-              <button
+              </button> */}
+              {/* <button
                 onClick={() => handleBuy(prices.tractor)}
                 className={`focus:outline-none h-20 aspect-h-1 aspect-w-1 ${isTractorBought ? "bg-green-500" : "bg-green-400 hover:bg-green-500"} rounded-lg`}><div className="flex flex-col justify-center">Unlock Tractor</div>
-              </button>
+              </button> */}
               <button
                 onClick={() => handleBuy(prices.water)}
                 className={`focus:outline-none aspect-h-1 aspect-w-1 ${isWaterBought ? "bg-green-500" : "bg-green-400 hover:bg-green-500"} rounded-lg`}>
@@ -81,7 +86,7 @@ const Store = () => {
                   <div className="mx-auto h-20 pt-6">
                     {isWaterBought ?  wellColorSVG : wellWhiteSVG}
                   </div>
-                  <div className="mt-2 text-white font-medium comfortaa">{isWaterBought ? "" : "ACORNS: 30"}</div>
+                  <div className="mt-2 text-white font-medium comfortaa">{isWaterBought ? "" : "ACORNS: 5"}</div>
                   <div className="text-sm font-medium text-white comfortaa px-2">{isWaterBought ? "Click the well to draw more water." : ""}</div>
                 </div>
               </button>
@@ -92,7 +97,7 @@ const Store = () => {
                   <div className="mx-auto">
                     {isBarnBought ?  barnColorSVG : barnWhiteSVG}
                   </div>
-                  <div className="mt-2 text-white font-medium comfortaa">{isBarnBought ? "" : "ACORNS: 30"}</div>
+                  <div className="mt-2 text-white font-medium comfortaa">{isBarnBought ? "" : "ACORNS: 10"}</div>
                   <div className="text-sm font-medium text-white comfortaa px-2">{isBarnBought ? "Store wood in your barn." : ""}</div>
                 </div>
               </button>
@@ -103,14 +108,14 @@ const Store = () => {
                   <div className="mx-auto">
                     {isSiloBought ?  siloColorSVG : siloWhiteSVG}
                   </div>
-                  <div className="mt-2 text-white font-medium comfortaa">{isSiloBought ? "" : "ACORNS: 30"}</div>
+                  <div className="mt-2 text-white font-medium comfortaa">{isSiloBought ? "" : "ACORNS: 15"}</div>
                   <div className="text-sm font-medium text-white comfortaa px-1">{isSiloBought ? "Winter is coming, seasons unlocked" : ""}</div>
                 </div>
               </button>
-              <button
+              {/* <button
                 onClick={() => handleBuy(prices.stars)}
                 className={`focus:outline-none h-20 aspect-h-1 aspect-w-1 ${isStarsBought ? "bg-green-500" : "bg-green-400 hover:bg-green-500"} rounded-lg`}><div className="flex flex-col justify-center">Unlock Stars</div>
-              </button>
+              </button> */}
             </div>
           <button className="block mx-auto w-min text-base comfortaa font-bold pt-1 focus:outline-none text-green-50 bg-green-600 hover:bg-green-700 px-2 font-medium rounded mt-4" aria-label="Exit Store" onClick={onExit} >EXIT</button>
           </div>
