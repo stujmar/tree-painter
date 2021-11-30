e5a7176893ca2a67c10ac14e3bce395edbbdc7a3
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-29 20:51:51 -0600

    [sm] got stars to persist in localStorage along with the tree and game stats

--- src/redux/skySlice.js
+++ src/redux/skySlice.js
@@ -1,6 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+const persistedState = localStorage.getItem('reduxState') 
+                       ? JSON.parse(localStorage.getItem('reduxState'))
+                       : {}
+
+const initialState = !!persistedState.sky ? persistedState.sky : {
     stars: [],
 };
 
