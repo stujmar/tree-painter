baa87c00efc28a0aaa490c7abdece5f44b770ab2
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-11-29 21:33:40 -0600

    [sm] removed some unnessesary top margin from the store description

--- src/components/gameBoard/store/Store.js
+++ src/components/gameBoard/store/Store.js
@@ -24,7 +24,7 @@ const Store = () => {
     return (
       <div className="absolute z-20 mt-4 w-full">
         <div className="w-11/12 md:w-8/12 bg-green-100 mx-auto mt-4 p-4 rounded-lg shadow-lg">
-          <div className="mt-2 block font-bold text-lg text-green-600 p-4 comfortaa text-center">Click to buy buildings which unlock new game features</div>
+          <div className="block font-bold text-lg text-green-600 p-4 comfortaa text-center">Click to buy buildings which unlock new game features</div>
           <div className="w-11/12 mx-auto grid gap-y-2 px-2 h-96 grid-cols-1 overflow-y-auto">
             {items}
           </div>
