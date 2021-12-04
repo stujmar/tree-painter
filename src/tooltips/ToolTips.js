da8201f2813f883009a83c33acdaae45cc1096e4
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-04 16:12:30 -0600

    [sm] added an arrow to the tip tour

--- src/tooltips/ToolTips.js
+++ src/tooltips/ToolTips.js
@@ -2,25 +2,40 @@ import React from 'react';
 
 const ToolTips = ({onClose}) => {
 
+const arrowBottomLeft = <div className="h-2 w-2 absolute -bottom-3" style={{ borderTop: "15px #BBF7D0 solid", borderLeft: "10px transparent solid", borderRight: "10px solid transparent"}} ></div>
+
   const tips = {
     "modes": {
       x: "left-2",
       y: "bottom-14",
-      content: "Click these buttons to switch modes!"
+      content: "Click these buttons to switch modes!",
+      arrow: arrowBottomLeft
+    },
+    "settings": {
+      x: "right-2",
+      y: "bottom-14",
+      content: "Click this button to open the settings menu!"
     }
   }
 
   return (
     <div 
-      className={`absolute ${tips["modes"].x} ${tips["modes"].y} z-50 flex bg-green-300 p-3 rounded`}>
-        <div className="comfortaa font-medium">
-          {tips["modes"].content}
-        </div>
-        <button 
-          type="button" 
-          className="focus:outline-none leading-4 pb-1 text-green-100 font-bold px-2 rounded-full ml-2 bg-green-600"
-          onClick={onClose}>x</button>
-      </div>
+      className={`absolute ${tips["modes"].x} ${tips["modes"].y} z-50 flex flex-col bg-green-200 shadow-lg p-2 rounded`}>
+          <div className="comfortaa font-medium">
+            {tips["modes"].content}
+          </div>
+        <div className="flex mt-2 justify-between">
+          <button 
+            type="button" 
+            className="focus:outline-none leading-4 pb-1 text-green-100 font-bold px-2 rounded bg-green-600"
+            onClick={onClose}>exit tour</button>
+          <button 
+            type="button" 
+            className="focus:outline-none leading-4 pb-1 text-green-100 font-bold px-2 rounded ml-2 bg-green-600"
+            onClick={onClose}>next tip -></button>
+          </div>
+          {tips["modes"].arrow}
+        </div> 
   )
 }
 
