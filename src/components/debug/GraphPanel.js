c9f3be3ee04e32168f2d38937ca2edd82472e09c
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-12-05 14:06:25 -0600

    [sm] add pused banner to the settings menu, shrunk the sandbox toggle

--- src/components/debug/GraphPanel.js
+++ src/components/debug/GraphPanel.js
@@ -28,23 +28,24 @@ const GraphPanel = ({toggleGraph}) => {
     }
 
     return (
-        <div className="absolute inset-0 z-50">
-                <div className="relative w-full h-full pt-10 bg-green-400">
-                    <div className="mx-auto flex justify-center py-4 items-center">
+        <div className="fixed inset-0 z-50">
+                <div className="relative w-full h-full bg-green-400">
+                <div className="p-2 pt-4 bg-green-200 text-green-700 font-bold text-center text-xl comfortaa">GAME IS PAUSED</div>
+                    <div className="mx-auto flex justify-start mt-4 ml-4 items-center">
                         <div className="font-bold text-green-800 text-xl comfortaa mr-4">Toggle Sandbox Mode</div>
                         <ToggleButton value={isSandbox} onClick={toggleSandbox} />
                     </div>
                     <button
                         className="fixed hover:bg-green-500 focus:outline-none left-2 top-2 rounded-full z-50 opacity-100"
                         onClick={toggleGraph}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 fill text-white" fill="none"  viewBox="0 0 24 24" stroke="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 fill bg-green-500 text-white rounded-full" fill="none"  viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
                         </svg>
                     </button> 
                     <Graph resetGame={reset} />
-                <form className="flex flex-col px-2">
+                <form className="flex flex-col px-2 max-w-4xl mx-auto mt-2">
                     <div className="comfortaa text-green-800 font-medium">Saved Game</div>
-                    <textarea cols="40" rows="5" value={localStorage.reduxState}></textarea>
+                    <textarea cols="30" rows="3" value={localStorage.reduxState}></textarea>
                     <div>
                         <button className="bg-green-600 px-1 rounded mt-2 text-white">submit</button>
                     </div>
--- src/components/debug/ToggleButton.js
+++ src/components/debug/ToggleButton.js
@@ -9,14 +9,14 @@ const ToggleButton = ({ value, onClick }) => {
 
     return (
         <button type="button" className="focus:outline-none select-none" onClick={handleClick}>
-        <div className="w-16 h-8 bg-green-700 shadow-lg rounded-full select-none">
+        <div className="w-12 h-6 bg-green-700 shadow-lg rounded-full select-none">
             <div 
                 style={{ 
-                transform: `${value ? 'translateX(32px)' : 'translateX(0px)'}`,
-                WebKitTransform: `${value ? 'translateX(32px)' : 'translateX(0px)'}`,
+                transform: `${value ? 'translateX(24px)' : 'translateX(0px)'}`,
+                WebKitTransform: `${value ? 'translateX(24px)' : 'translateX(0px)'}`,
                 background: `${value ? '#A3E635' : '#E4E4E7'}`
                 }}
-                className="w-8 h-8 focus:outline-none ease-in-out duration-200 shadow-lg transform rounded-full select-none"></div>
+                className="w-6 h-6 focus:outline-none ease-in-out duration-200 shadow-lg transform rounded-full select-none"></div>
         </div>
         </button>
     )
