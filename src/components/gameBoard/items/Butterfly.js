32d125ec31baec461bf8ca251d9bdc55e4e892a6
Stuart Marsh <stujmar@gmail.com>
2021-12-22 21:52:47 -0600

    [sm] built out butterfly as a button comp with a click handler and awareness of mode

--- src/components/gameBoard/items/Butterfly.js
+++ src/components/gameBoard/items/Butterfly.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectMode } from '../../../redux/gameSlice';
 
-const Butterfly = () => {
+const Butterfly = ({data}) => {
+  let mode = useSelector(selectMode);
+  const handleClick = () => {
+    console.log("you clicked a butterfly!", mode);
+  }
   return (
-    <></>
+    <button
+      className="absolute focus:outline-none"
+      onClick={() => handleClick()}
+      style={{
+        left: `${data.x}%`, 
+        top: `${data.y -3}%`, 
+        width: "20px",
+        height: "20px",
+        }}
+    >X</button>
   )
 }
 
