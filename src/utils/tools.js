79c64e804697f619500201350a83cd2959842221
Stuart Marsh <stujmar@gmail.com>
2021-12-25 19:36:26 -0500

    [sm] added the ablity for the get SineY function to accept an x offset

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,9 +1,9 @@
 // get y position of a sine wave based on x
-const getSineY = (x) => {
+const getSineY = (x, offset) => {
   let amplitude = 20;
   let frequency = 15;
   let height = 100;
-  return height/2 + amplitude * Math.sin(x/frequency);
+  return height/2 + amplitude * Math.sin((x + offset)/frequency);
 }
 
 export { getSineY };
